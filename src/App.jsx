c0c14f91c809822c457a0b2e9ea3ee33fc4b362b
maike1983 <Maikevandartel@outlook.com>
2026-04-19 15:59:02 +0200

    Laatste versie voor inleveren

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,6 @@ function App() {
 
     // console.log(mostSoldFirst(inventory));
 
-
     function handleMostSoldClick() {
         console.log(mostSoldFirst(inventory));
     }
@@ -36,69 +35,81 @@ function App() {
         console.log(bestSportsFirst(inventory));
     }
 
-return (
-    <>
-        <header>
-            <h1>Tech it easy dashboard</h1>
-        </header>
-        <main>
-            <section>
-                <h2>Verkoopoverzicht</h2>
-                <p className="green-text">Aantal verkochte producten: {calculateSoldProducts(inventory)}</p>
-                <p className="blue-text">Aantal ingekochte producten: {calculateTotalStock(inventory)}</p>
-                <p className="red-text">Aantal te verkopen producten: {calculateRemainingStock(inventory)}</p>
-            </section>
-            <section>
-                <h2>Best verkochte tv</h2>
-                <img id="best-seller-img" src={bestSellingTv.sourceImg} alt="Best verkochte tv"/>
-                <p>{formatProductName(bestSellingTv)}</p>
-                <p>{formatPrice(bestSellingTv)}</p>
-                <p>{formatSize(bestSellingTv)}</p>
+    return (
+        <>
+            <header>
+                <h1>Tech it easy dashboard</h1>
+            </header>
+            <main>
+                <section>
+                    <h2>Verkoopoverzicht</h2>
+                    <div className="statistics">
+                        <div className="statistics-box" id="numbers-sold">
+                            <p>Aantal verkochte producten</p>
+                            <p>{calculateSoldProducts(inventory)}</p>
+                        </div>
 
-                <p>
-                    <img className="icon" src="src/assets/check.png" alt="Aanwezig"/> wifi
-                    <img className="icon" src="src/assets/minus.png" alt="Afwezig"/> speech
-                    <img className="icon" src="src/assets/check.png" alt="Aanwezig"/> hdr
-                    <img className="icon" src="src/assets/check.png" alt="Aanwezig"/> bluetooth
-                    <img className="icon" src="src/assets/minus.png" alt="Afwezig"/> ambilight
-                </p>
-            </section>
-            <section>
-                <h2>Alle tv's</h2>
-                <button type="button" onClick={handleMostSoldClick}>Meest verkocht eerst</button>
-                <button type="button" onClick={handleCheapestClick}>Goedkoopste eerst</button>
-                <button type="button" onClick={handleBestSportsClick}>Meest geschikt voor sport eerst</button>
-                {/*{tvNames(inventory).map((tvName) => {*/}
-                {/*    return <p key={tvName}>{tvName}</p>*/}
-                {/*}*/}
-                {/*)}*/}
-                {inventory.map((tv) => (
-                    <div key={tv.type}>
-                        <img className="tv-images" src={tv.sourceImg} alt={tv.name}/>
-                        <p>{formatProductName(tv)}</p>
-                        <p>{formatPrice(tv)}</p>
-                        <p>{formatSize(tv)}</p>
-                        <p>
-                            {tv.options.map((option) => (
-                                <span key={option.name}>
+                        <div className="statistics-box" id="numbers-bought">
+                            <p>Aantal ingekochte producten</p>
+                            <p>{calculateTotalStock(inventory)}</p>
+                        </div>
+                        <div className="statistics-box" id="numbers-stock">
+                            <p>Aantal te verkopen producten</p>
+                            <p>{calculateRemainingStock(inventory)}</p>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <h2>Best verkochte tv</h2>
+                    <img id="best-seller-img" src={bestSellingTv.sourceImg} alt="Best verkochte tv"/>
+                    <p>{formatProductName(bestSellingTv)}</p>
+                    <p>{formatPrice(bestSellingTv)}</p>
+                    <p>{formatSize(bestSellingTv)}</p>
+
+                    <p>
+                        <img className="icon" src="src/assets/check.png" alt="Aanwezig"/> wifi
+                        <img className="icon" src="src/assets/minus.png" alt="Afwezig"/> speech
+                        <img className="icon" src="src/assets/check.png" alt="Aanwezig"/> hdr
+                        <img className="icon" src="src/assets/check.png" alt="Aanwezig"/> bluetooth
+                        <img className="icon" src="src/assets/minus.png" alt="Afwezig"/> ambilight
+                    </p>
+                </section>
+                <section>
+                    <h2>Alle tv's</h2>
+                    <button type="button" onClick={handleMostSoldClick}>Meest verkocht eerst</button>
+                    <button type="button" onClick={handleCheapestClick}>Goedkoopste eerst</button>
+                    <button type="button" onClick={handleBestSportsClick}>Meest geschikt voor sport eerst</button>
+                    {/*{tvNames(inventory).map((tvName) => {*/}
+                    {/*    return <p key={tvName}>{tvName}</p>*/}
+                    {/*}*/}
+                    {/*)}*/}
+                    {inventory.map((tv) => (
+                        <div key={tv.type}>
+                            <img className="tv-images" src={tv.sourceImg} alt={tv.name}/>
+                            <p>{formatProductName(tv)}</p>
+                            <p>{formatPrice(tv)}</p>
+                            <p>{formatSize(tv)}</p>
+                            <p>
+                                {tv.options.map((option) => (
+                                    <span key={option.name}>
                                 {option.applicable ? (
                                     <img className="icon" src="src/assets/check.png" alt="Aanwezig"/>
                                 ) : (
                                     <img className="icon" src="src/assets/minus.png" alt="Afwezig"/>
                                 )}
-                                    {option.name}
+                                        {option.name}
                                 </span>
-                            ))}
-                        </p>
-                    </div>
-                ))}
-            </section>
-        </main>
-        <footer>
-            <p>©️2026 Tech It Easy</p>
-        </footer>
-    </>
-);
+                                ))}
+                            </p>
+                        </div>
+                    ))}
+                </section>
+            </main>
+            <footer>
+                <p>©️2026 Tech It Easy</p>
+            </footer>
+        </>
+    );
 }
 
 export default App
